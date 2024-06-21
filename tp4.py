c7f093923c3ca95653903a5a51f466a6503d92ed
Anaholcman <anitaholcman@gmail.com>
2024-06-21 10:55:39 -0300

    update

--- tp4.py
+++ tp4.py
@@ -69,7 +69,7 @@ U, S, VT = np.linalg.svd(A, full_matrices=False)
 x_svd = np.dot(VT.T, np.dot(np.linalg.inv(np.diag(S)), np.dot(U.T, b)))
 error_svd = np.linalg.norm(np.dot(A, x_svd) - b)
 
-# Graficar la evolución del error
+# Grafico la evolución del error
 plt.plot(range(len(errors)), errors, label='F(x)', color='darkgreen')
 plt.plot(range(len(errors_reg)), errors_reg, label='F2(x) con Regularización', color='purple')
 plt.axhline(y=error_svd, color='orange', linestyle='--', label='SVD')
@@ -81,7 +81,7 @@ plt.grid(True)
 plt.yscale('log')
 plt.show()
 
-# Graficar la comparación de soluciones
+# Grafico la comparación de soluciones
 plt.plot(x_solution, label='Gradiente Descendente F(x)')
 plt.plot(x_solution_reg, label='Gradiente Descendente F2(x)', linestyle='dashed')
 plt.plot(x_svd, label='SVD', linestyle='dotted', linewidth=1.5)
@@ -91,14 +91,15 @@ plt.title('Comparación de Soluciones')
 plt.legend()
 plt.show()
 
-# Graficar la norma 2 de x en función de las iteraciones
+# Grafico la norma 2 de x en función de las iteraciones
 plt.plot(range(len(norms)), norms, label='Norma 2 de x - F(x)', color='darkgreen')
 plt.plot(range(len(norms_reg)), norms_reg, label='Norma 2 de x - F2(x)', color='purple')
 plt.xlabel('Iteraciones')
-plt.ylabel('Norma 2 de x')
-plt.title('Norma 2 de x en función de iteraciones')
+plt.ylabel(r'$\|x\|_2$')
+plt.title(r'$\|x\|_2$ en función de iteraciones')
 plt.legend()
 plt.grid(True)
+plt.yscale('log')
 plt.show()
 
 # Análisis del impacto de la regularización L2
